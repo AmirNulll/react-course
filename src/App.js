272d50a68babc4d5d3d50a5daeb0080a3a3a8147
nightknight <nightknightamir@gmail.com>
2024-03-16 13:24:23 +0330

    create backdrop and import modal and backdrop to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import Backdrop from "./Components/Backdrop";
 import Todo from "./Components/Todo";
+import Modal from "./Components/Modal";
+import Backdrop from "./Components/Backdrop";
 
 function App() {
   return (
     <div>
       <h1>My todo</h1>
-     
+
       <Todo text="Hello" />
-      <Todo text='World' />
+      <Todo text="World" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
